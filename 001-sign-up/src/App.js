02ef24b3b68d1dc569e0d3b081497a17805ef2e8
Lekan <lekan.nicholson@gmail.com>
2019-10-21 14:56:57 -0400

    webpack error, default.a.createClass is not a function

--- 001-sign-up/src/App.js
+++ 001-sign-up/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+
 
 var Input = React.createClass({
     render: function() {
