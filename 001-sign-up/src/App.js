37350a925a1ab9515517fe21a0244ac7b75f2d3f
Lekan <lekan.nicholson@gmail.com>
2019-10-24 15:53:30 -0400

    create simple inputs and button for sign up page

--- 001-sign-up/src/App.js
+++ 001-sign-up/src/App.js
@@ -1,68 +1,52 @@
 import React from 'react';
 import './App.css';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-
-
-var Input = React.createClass({
-    render: function() {
-        return (
-            <div className="Input">
-                <input id={this.props.name} autocomplete="false" required type={this.props.type} placeholder={this.props.placeholder} />
-                <label for={this.props.name}></label>
-            </div>
-        );
+class App extends React.Component {
+    state = {
+        firstName: "John",
+        lastName: "Henry"
     }
-});
 
-var Modal = React.createClass({
-    render: function() {
+    handleFirstNameChange = event => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    handleLastNameChange = event => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+        let dataArray = this.state.submittedData.concat(formData)
+        this.setState({submittedData: dataArray})
+    }
+
+
+    render() {
         return (
-            <div className="Modal">
-                <form onSubmit={this.props.onSubmit} className="ModalForm">
-                    <Input id="name" type="text" placeholder="Jack-Edward Oliver" />
-                    <Input id="username" type="email" placeholder="mrjackolai@gmail.com" />
-                    <Input id="password" type="password" placeholder="password" />
-                    <button>Log in <i className="fa fa-fw fa-chevron-right"></i></button>
+            <div>
+                <form onSubmit={event => this.handleSubmit(event)}>
+                    <input
+                        type="text"
+                        onChange={event => this.handleFirstNameChange(event)}
+                        value={this.state.firstName}
+                    />
+                    <input
+                        type="text"
+                        onChange={event => this.handleLastNameChange(event)}
+                        value={this.state.lastName}
+                    />
+                    <input type="submit" />
                 </form>
-            </div>
-        );
-    }
-});
-
-var App = React.createClass({
-
-    getInitialState: function() {
-        return { mounted: false };
-    },
-
-    componentDidMount: function() {
-        this.setState({ mounted: true });
-    },
-
-    handleSubmit: function(e) {
-        this.setState({ mounted: false });
-        e.preventDefault();
-    },
-
-    render: function() {
-        var child;
-
-        if(this.state.mounted) {
-            child = (<Modal onSubmit={this.handleSubmit} />);
-        }
-
-        return(
-            <div className="App">
-                return(
-                <div className="App">
-                    <ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
-                        {child}
-                    </ReactCSSTransitionGroup>
-                </div>
             </div>
         )
     }
-});
+}
+
 
 export default App;
