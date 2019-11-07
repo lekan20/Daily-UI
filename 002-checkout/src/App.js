80583998464a82711e0930c0b7b8c6151a809597
Lekan <lekan.nicholson@gmail.com>
2019-11-07 14:24:05 -0500

    starting project

---
+++ 002-checkout/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Overlay" style={{'background': 'url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg)'}}>
+
+    </div>
+  );
+}
+
+export default App;
