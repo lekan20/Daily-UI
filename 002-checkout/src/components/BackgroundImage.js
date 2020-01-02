9dfd05a17f7a28f2fa6dc4031c4e66b1784b8ce7
Lekan <lekan.nicholson@gmail.com>
2020-01-02 08:01:56 -0500

    Get image file to render on browser

--- 002-checkout/src/components/BackgroundImage.js
+++ 002-checkout/src/components/BackgroundImage.js
@@ -2,9 +2,7 @@ import React from "react";
 
 class BackgroundImage extends React.Component {
   render() {
-    return <div>
-      <img src={"https://www.wework.com/ideas/wp-content/uploads/sites/4/2019/07/Web_72DPI-20180717-WeWork-Constellation-Common-Areas-Wide-3-1120x630.jpg"}>
-      </div>
+    return <div><img src={'images/picture'} alt={"hello"}/></div>
   }
 }
 
