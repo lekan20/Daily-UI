27a8e080296fc232bb268e2e33704c2765dccb26
Lekan <lekan.nicholson@gmail.com>
2020-01-03 10:54:18 -0500

    Update payment Information form

--- 002-checkout/src/components/PaymentInformation.js
+++ 002-checkout/src/components/PaymentInformation.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 class PaymentInformation extends React.Component {
     render() {
-        return <div>Payment Information</div>
+        return <form>
+            <label>
+                Name on Credit Card:
+                <input type="text" name="name" />
+                Credit Card Number
+                <input type="text" name="number" />
+                Expires on:
+                <select>
+                    <option value="Janurary">January </option>
+                    <option value="February">February </option>
+                    <option value="March">March </option>
+                    <option value="April">April </option>
+                    <option value="May">May </option>
+                    <option value="June">June </option>
+                    <option value="July">July </option>
+                    <option value="August">August </option>
+                    <option value="September">September </option>
+                    <option value="Octorber">October </option>
+                    <option value="November">November </option>
+                    <option value="December">December </option>
+                </select>
+            </label>
+        </form>
     }
 }
 
